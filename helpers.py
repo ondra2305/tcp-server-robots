abe7f070bfe6bacac886891deb191c3df8bc1226
Ondřej Svoboda <svoboo30@fit.cvut.cz>
2023-04-27 13:25:10 +0200

    Login error handling, basic moving

--- helpers.py
+++ helpers.py
@@ -1,5 +1,5 @@
 from config import *
-
+import math
 
 def calc_client_hash(username):
     if len(username) > 20:
@@ -30,3 +30,94 @@ def calc_srv_key(cl_hash, kid):
     elif kid == 4:
         res = (cl_hash + S_KEY4) % 65536
     return res
+
+
+def calc_cl_key(cl_hash, kid):
+    res = None
+    if kid == 0:
+        res = (cl_hash + C_KEY0) % 65536
+    elif kid == 1:
+        res = (cl_hash + C_KEY1) % 65536
+    elif kid == 2:
+        res = (cl_hash + C_KEY2) % 65536
+    elif kid == 3:
+        res = (cl_hash + C_KEY3) % 65536
+    elif kid == 4:
+        res = (cl_hash + C_KEY4) % 65536
+    return res
+
+
+class Robot:
+    def __init__(self):
+        self.direction = None
+        self.xPos = None
+        self.yPos = None
+        self.first_move = True
+
+    def move_to_0(self, x, y):
+        y = y.split('\a\b')[0]
+        print(f'My pos: {x},{y}')
+
+        self.xPos = int(x)
+        self.yPos = int(y)
+
+        distance = math.sqrt(self.xPos**2 + self.yPos**2)
+
+        if distance == 0:
+            return SERVER_PICK_UP
+
+        if self.first_move:
+            self.first_move = False
+            return SERVER_MOVE
+
+        if self.direction is None:
+            if self.xPos > 0:
+                self.direction = 'W'
+            elif self.xPos < 0:
+                self.direction = 'E'
+            elif self.yPos > 0:
+                self.direction = 'S'
+            elif self.yPos < 0:
+                self.direction = 'N'
+
+        if self.direction == 'E':
+            if self.xPos > 0:
+                return SERVER_MOVE
+            elif self.yPos < 0:
+                self.direction = 'N'
+                return SERVER_TURN_LEFT
+            else:
+                self.direction = 'S'
+                return SERVER_TURN_RIGHT
+
+        elif self.direction == 'W':
+            if self.xPos < 0:
+                return SERVER_MOVE
+            elif self.yPos > 0:
+                self.direction = 'S'
+                return SERVER_TURN_LEFT
+            else:
+                self.direction = 'N'
+                return SERVER_TURN_RIGHT
+
+        elif self.direction == 'N':
+            if self.yPos > 0:
+                return SERVER_MOVE
+            elif self.xPos > 0:
+                self.direction = 'W'
+                return SERVER_TURN_LEFT
+            else:
+                self.direction = 'E'
+                return SERVER_TURN_RIGHT
+
+        elif self.direction == 'S':
+            if self.yPos < 0:
+                return SERVER_MOVE
+            elif self.xPos < 0:
+                self.direction = 'E'
+                return SERVER_TURN_LEFT
+            else:
+                self.direction = 'W'
+                return SERVER_TURN_RIGHT
+
+        return SERVER_MOVE
--- server.py
+++ server.py
@@ -1,22 +1,9 @@
 import socket
 import threading
+import numpy as np
+
 from config import *
 from helpers import *
-
-
-def calc_cl_key(cl_hash, kid):
-    res = None
-    if kid == 0:
-        res = (cl_hash + C_KEY0) % 65536
-    elif kid == 1:
-        res = (cl_hash + C_KEY1) % 65536
-    elif kid == 2:
-        res = (cl_hash + C_KEY2) % 65536
-    elif kid == 3:
-        res = (cl_hash + C_KEY3) % 65536
-    elif kid == 4:
-        res = (cl_hash + C_KEY4) % 65536
-    return res
 
 
 class Server:
@@ -43,39 +30,52 @@ class Server:
 
     def handle_cl_conn(self, conn, addr, thread_id):
         print(f"New connection from: {addr}.")
-
+        robot = Robot()
 
         conn.settimeout(1)
         connected = True
+        message = ""
         while connected:
-            data = conn.recv(1024).decode(MSG_FORMAT)
-            print(f"Message from [{addr}]: {data}")
-            response = self.process_data(data, thread_id)
-            conn.sendall(response.encode(MSG_FORMAT))
-            if (response == SERVER_LOGIN_FAILED) or (response == SERVER_KEY_OUT_OF_RANGE_ERROR):
+            try:
+                data = conn.recv(1024).decode(MSG_FORMAT)
+                if not data:
+                    break
+                message += data
+                if message.endswith('\a\b'):
+                    #message = message[:-2]
+                    print(f"Message from [{addr}]: {message}")
+                    response = self.process_data(message, thread_id, robot)
+                    conn.sendall(response.encode(MSG_FORMAT))
+                    if response in [SERVER_LOGIN_FAILED, SERVER_KEY_OUT_OF_RANGE_ERROR, SERVER_LOGIC_ERROR,
+                                    SERVER_SYNTAX_ERROR, SERVER_LOGOUT]:
+                        connected = False
+                        break
+                    elif response == SERVER_OK:
+                        conn.sendall(SERVER_MOVE.encode(MSG_FORMAT))
+                    message = ""
+            except socket.timeout:
+                print(f"Connection timed out for {addr}.")
                 break
-            elif (response == SERVER_LOGIC_ERROR) or (response == SERVER_SYNTAX_ERROR):
-                break
-            elif (response == SERVER_OK):
-                conn.sendall(SERVER_MOVE.encode(MSG_FORMAT))
 
-        #conn.sendall(SERVER_LOGOUT.encode(MSG_FORMAT))
         conn.close()
         print(f"Client at {addr} disconnected.")
 
-    def process_data(self, data, thread_id):
+    def process_data(self, data, thread_id, robot):
         messages = data.split('\a\b')
         response = SERVER_SYNTAX_ERROR
 
         state = self.connected_clients[thread_id]['con_state']
         username = self.connected_clients[thread_id]['username']
 
-        if messages[1] != '':
+        message_count = len(messages)
+        msg = messages[0]
+        if message_count > 1 and messages[1] != '':
             return response
 
         if state == 'connected':
-            if len(messages) > 1 and not hasattr(self.process_data, 'username'):
-                username = messages[0]
+            username = messages[0]
+
+            if len(messages) > 1 and not hasattr(self.process_data, 'username') and len(username) <= 18:
                 print(f'{username} connected')
                 response = SERVER_KEY_REQUEST
                 self.connected_clients[thread_id]['username'] = username
@@ -84,29 +84,43 @@ class Server:
                 response = SERVER_SYNTAX_ERROR
         elif state == 'key_req':
             print(f'{username}: Recv {messages[0]} key')
-            kid = int(messages[0])
-            self.connected_clients[thread_id]['cl_hash'] = calc_client_hash(username)
-            self.connected_clients[thread_id]['key_id'] = kid
-            if kid > 4 or kid < 0:
+            if not np.char.isnumeric(messages[0]):
                 self.connected_clients[thread_id]['con_state'] = 'keyerr'
-                response = SERVER_KEY_OUT_OF_RANGE_ERROR
+                response = SERVER_SYNTAX_ERROR
             else:
-                self.connected_clients[thread_id]['con_state'] = 'key_sent'
-                res = calc_srv_key(self.connected_clients[thread_id]['cl_hash'], kid)
-                response = f"{res}\a\b"
+                kid = int(messages[0])
+                print(f'KID: {kid}')
+                self.connected_clients[thread_id]['cl_hash'] = calc_client_hash(username)
+                self.connected_clients[thread_id]['key_id'] = kid
+                if 4 >= kid >= 0:
+                    self.connected_clients[thread_id]['con_state'] = 'key_sent'
+                    res = calc_srv_key(self.connected_clients[thread_id]['cl_hash'], kid)
+                    response = f"{res}\a\b"
+                else:
+                    self.connected_clients[thread_id]['con_state'] = 'keyerr'
+                    response = SERVER_KEY_OUT_OF_RANGE_ERROR
         elif state == 'key_sent':
             hashcalc = self.connected_clients[thread_id]['cl_hash']
             recvkey = int(messages[0])
             kid = self.connected_clients[thread_id]['key_id']
             print(f'Cl hash: _{hashcalc}_')
             print(f'Key recv: _{messages[0]}_')
-            if calc_cl_key(hashcalc, kid) == recvkey:
-                self.connected_clients[thread_id]['con_state'] = 'login_complete'
+            if recvkey > 99999:
+                response = SERVER_SYNTAX_ERROR
+            elif calc_cl_key(hashcalc, kid) == recvkey:
+                self.connected_clients[thread_id]['con_state'] = 'move'
                 response = SERVER_OK
             else:
                 response = SERVER_LOGIN_FAILED
-        elif state == 'login_complete':
-            response = SERVER_MOVE
+        elif state == 'move':
+            print(f'Move: {msg}')
+            msg = data.split(' ')
+            response = robot.move_to_0(msg[1], msg[2])
+            if response == SERVER_PICK_UP:
+                self.connected_clients[thread_id]['con_state'] = 'pickup'
+        elif state == 'pickup':
+            print(f'Tajny vzkaz: {msg}')
+            response = SERVER_LOGOUT
         else:
             response = SERVER_SYNTAX_ERROR
 
